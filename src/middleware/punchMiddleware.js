3240d1bc6751bd2dbf74d554dab9d00dbbd0de07
amit <amit.dhiman@nxtgenvirtue.com>
2024-04-25 16:13:24 +0530

    ip

--- src/middleware/punchMiddleware.js
+++ src/middleware/punchMiddleware.js
@@ -5,7 +5,7 @@ const restrictAccessByIP = async (req, res, next) => {
         const response = await axios.get('https://api64.ipify.org?format=json');
         const ipAddress = response.data.ip;
         console.log(ipAddress)
-        const allowedIPs = ['49.249.21.166', '122.186.85.262'];
+        const allowedIPs = ['49.249.21.166', '122.186.85.262', '156.67.210.176'];
         const clientIP = ipAddress; // Get the client's IP address
         if (allowedIPs.includes(clientIP)) {
             // If client's IP is allowed, proceed to the next middleware/route handler
